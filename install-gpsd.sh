8626dc761ca8ebd61a802fca4cd9c2e0ecd6e3c0
Shawn Kim <tkim1@email.arizona.edu>
2025-05-22 22:02:55 -0700

    update

--- install-gpsd.sh
+++ install-gpsd.sh
@@ -1,4 +1,4 @@
-sudo apt install gpsd gpsd-clients
+sudo apt install gpsd gpsd-clients -y
 sudo systemctl stop gpsd.socket
 sudo systemctl disable gpsd.socket
 sudo gpsd /dev/ttyACM0 -F /var/run/gpsd.sock
