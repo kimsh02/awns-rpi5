8a8b0e926ee222cc62227a190eccc3e89250ef74
Shawn Kim <tkim1@email.arizona.edu>
2025-05-27 00:33:52 -0700

    update

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 # Install CMake to build executable
 sudo apt install cmake -y
 
-# Install and set up GPS drivers
-sudo apt install gpsd gpsd-clients -y
+# Install and set up GPS drivers/libraries
+sudo apt install gpsd gpsd-clients libgps-dev -y
 sudo systemctl stop gpsd.socket
 sudo systemctl disable gpsd.socket
 sudo gpsd /dev/ttyACM0 -F /var/run/gpsd.sock
