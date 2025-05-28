43d71f5ff3c8293db72e9eb518c5bc22fa8a5740
Shawn Kim <tkim1@email.arizona.edu>
2025-05-27 21:50:14 -0700

    update

--- app/main.cpp
+++ app/main.cpp
@@ -11,10 +11,18 @@ int main()
 	GPSClient gps{};
 	gps.connect();
 	gps.startStream();
-	GPSFix fix = gps.readFix().value_or(GPSFix{ 0, 0 });
 
-	std::cout << "Lat: " << fix.latitude << ", Lon: " << fix.longitude
-		  << "\n";
+	int ctr = 0;
+	while (true) {
+		GPSFix fix = gps.readFix().value_or(GPSFix{ 0, 0 });
+		std::cout << "Lat: " << fix.latitude
+			  << ", Lon: " << fix.longitude << "\n";
+		ctr++;
+		if (fix.latitude) {
+			break;
+		}
+	}
+	std::cout << ctr << " tries\n";
 
 	gps.stopStream();
 	return 0;
