b4a12f0f5bf9bd74bb82889ca259054c463e0be4
Shawn Kim <tkim1@email.arizona.edu>
2025-05-28 22:06:25 -0700

    update

--- app/main.cpp
+++ app/main.cpp
@@ -15,6 +15,12 @@ int main()
 	GPSFix fix = gps.waitReadFix().value_or(GPSFix{ 0, 0 });
 	std::cout << "Lat: " << fix.latitude << ", Lon: " << fix.longitude
 		  << "\n";
+	GPSFix fix1 = gps.waitReadFix().value_or(GPSFix{ 0, 0 });
+	std::cout << "Lat: " << fix1.latitude << ", Lon: " << fix1.longitude
+		  << "\n";
+	GPSFix fix2 = gps.waitReadFix().value_or(GPSFix{ 0, 0 });
+	std::cout << "Lat: " << fix2.latitude << ", Lon: " << fix2.longitude
+		  << "\n";
 
 	gps.stopStream();
 	return 0;
