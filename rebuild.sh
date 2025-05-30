ff02e8880b6be3e11df57c497ef9d8b54c7201c6
Shawn Kim <tkim1@email.arizona.edu>
2025-05-30 08:14:47 -0700

    update

---
+++ rebuild.sh
@@ -0,0 +1,6 @@
+# Script to pull and rebuild executable on Raspberry Pi
+git pull
+cd app/build/
+cmake -DCMAKE_BUILD_TYPE=release -DENABLE_COVERAGE=OFF ..
+cmake --build .
+sudo mv awns-rpi5 /usr/local/bin/
