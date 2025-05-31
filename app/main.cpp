211869842f43321ee54bdb91f37915bd06686ae2
Shawn Kim <tkim1@email.arizona.edu>
2025-05-31 05:36:27 -0700

    update

--- app/main.cpp
+++ app/main.cpp
@@ -1,11 +1,17 @@
 extern "C" {
-#include <gps.h>
+#include <tsp.h>
+#include <util.h>
 }
 
 #include "navigator.hpp"
 
 int main()
 {
+	// The Concorde library is intended for CLI use, but this proves you can include headers
+	CCdatagroup dat;
+	dat.x = nullptr;
+	dat.y = nullptr;
+
 	/* Instantiate and run navigator */
 	Navigator nav{};
 	nav.run();
