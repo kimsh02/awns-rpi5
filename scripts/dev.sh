c7b7e77aaa67ee2e94caf8eee8c6539600bdccb6
Shawn Kim <tkim1@email.arizona.edu>
2025-06-06 16:04:44 -0700

    update

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,5 +1,6 @@
 # Script to pull and rebuild executable on Raspberry Pi
 git pull
+sudo ./scripts/python-plot.sh
 cd app/build/
 cmake -DCMAKE_BUILD_TYPE=release -DENABLE_COVERAGE=OFF ..
 cmake --build .
